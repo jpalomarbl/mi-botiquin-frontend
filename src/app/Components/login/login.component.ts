94fff5b6dbf8be9ac33f742e4eb60960862cfb57
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-09 10:38:15 +0100

    Installed Angular Material

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,34 +1,32 @@
 import { Component } from '@angular/core';
-// import { Store } from '@ngrx/store';
+import { Store } from '@ngrx/store';
 
-// import { FormsModule } from '../../Modules/forms/forms.module';
+import { FormsModule } from '../../Modules/forms/forms.module';
 
-// import { login } from '../../auth/actions/auth.actions';
-// import { LoginDTO } from '../../auth/models/auth.dto';
-// import * as selectors from '../../auth/selectors/auth.selectors';
-// import { Observable } from 'rxjs';
+import { login } from '../../auth/actions/auth.actions';
+import { LoginDTO } from '../../auth/models/auth.dto';
+import * as selectors from '../../auth/selectors/auth.selectors';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  // cretentials: LoginDTO;
+  cretentials: LoginDTO;
 
-  // loading$: Observable<boolean>;
-  // error$: Observable<string | null>;
+  loading$: Observable<boolean>;
+  error$: Observable<string | null>;
 
-  // constructor(private store: Store) {
-  //   this.cretentials = { email: '', password: '' };
+  constructor(private store: Store) {
+    this.cretentials = { email: '', password: '' };
 
-  //   this.loading$ = this.store.select(selectors.selectAuthLoading);
-  //   this.error$ = this.store.select(selectors.selectAuthError);
-  // }
+    this.loading$ = this.store.select(selectors.selectAuthLoading);
+    this.error$ = this.store.select(selectors.selectAuthError);
+  }
 
-  // login(): void {
-  //   this.store.dispatch(login({ credentials: this.cretentials }));
-  // }
+  login(): void {
+    this.store.dispatch(login({ credentials: this.cretentials }));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { authReducer } from './auth/reducers/auth.reducer';
 
+import { LoginComponent } from './Components/login/login.component';
+import { FormsModule } from './Modules/forms/forms.module';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LoginComponent],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({ auth: authReducer })
+    StoreModule.forRoot({ auth: authReducer }),
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
