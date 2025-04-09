30525839097b6911eb3eadf32933d54c36185a88
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-09 11:16:17 +0100

    Added effects module to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StoreModule } from '@ngrx/store';
-import { authReducer } from './auth/reducers/auth.reducer';
 
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { HttpClientModule } from '@angular/common/http';
+
+import { authReducer } from './auth/reducers/auth.reducer';
+import { AuthEffects } from './auth/effects/auth.effects';
 import { LoginComponent } from './Components/login/login.component';
 import { FormsModule } from './Modules/forms/forms.module';
 
@@ -17,7 +21,9 @@ import { FormsModule } from './Modules/forms/forms.module';
     FormsModule,
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     StoreModule.forRoot({ auth: authReducer }),
+    EffectsModule.forRoot([AuthEffects]),
     BrowserAnimationsModule,
   ],
   providers: [],
