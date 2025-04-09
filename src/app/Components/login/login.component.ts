be14595937eb1450b669607482017ffeab285521
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-09 11:18:11 +0100

    Implemented login form

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 
-import { FormsModule } from '../../Modules/forms/forms.module';
-
 import { login } from '../../auth/actions/auth.actions';
 import { LoginDTO } from '../../auth/models/auth.dto';
 import * as selectors from '../../auth/selectors/auth.selectors';
 import { Observable } from 'rxjs';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -14,19 +13,40 @@ import { Observable } from 'rxjs';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  cretentials: LoginDTO;
+  credentials: LoginDTO;
+
+  email: FormControl;
+  password: FormControl;
+  loginForm: FormGroup;
 
   loading$: Observable<boolean>;
   error$: Observable<string | null>;
 
   constructor(private store: Store) {
-    this.cretentials = { email: '', password: '' };
+    this.credentials = { email: '', password: '' };
+
+    this.email = new FormControl(this.credentials.email);
+    this.password = new FormControl(this.credentials.password);
+    this.loginForm = new FormGroup({
+      email: this.email,
+      password: this.password
+    });
 
     this.loading$ = this.store.select(selectors.selectAuthLoading);
     this.error$ = this.store.select(selectors.selectAuthError);
   }
 
-  login(): void {
-    this.store.dispatch(login({ credentials: this.cretentials }));
+  submitLogin(): void {
+    this.credentials.email = this.email.value;
+    this.credentials.password = this.password.value;
+
+    this.store.dispatch(login({ credentials: this.credentials }));
+
+    console.log(
+      'User: ',
+      this.credentials.email,
+      '\nPassword: ',
+      this.credentials.password
+    );
   }
 }
