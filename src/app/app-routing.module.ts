0ee7a536bd015872b8ae624419fc92addd469ea5
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-11 12:13:59 +0100

    Added OAuth Login routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './Components/login/login.component';
+import { OAuthSuccessComponent } from './Components/oauth-success/oauth-success.component';
+import { OAuthRedirectGuard } from './Guards/o-auth-redirect.guard';
+import { OauthErrorComponent } from './Components/oauth-error/oauth-error.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'oauth-success',
+    canActivate: [OAuthRedirectGuard],
+    component: OAuthSuccessComponent
+  },
+  {
+    path: 'oauth-error',
+    component: OauthErrorComponent
   },
   {
     path: '',
