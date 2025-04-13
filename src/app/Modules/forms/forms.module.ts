b72a4120f1334e6a6327452338d1690b7a3faad7
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu>
2025-04-13 08:32:14 +0100

    Added select input to Forms Module

--- src/app/Modules/forms/forms.module.ts
+++ src/app/Modules/forms/forms.module.ts
@@ -6,19 +6,25 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule
   ],
   exports: [
     CommonModule,
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
-  ]
+    MatButtonModule,
+    MatSelectModule
+  ],
 })
-export class FormsModule { }
+export class FormsModule {}
