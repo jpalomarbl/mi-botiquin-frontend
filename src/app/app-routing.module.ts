a76d59f304128be31804d9778df7565876bc59df
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu>
2025-04-13 07:41:11 +0100

    Added register route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,30 +5,35 @@ import { LoginComponent } from './Components/login/login.component';
 import { OAuthSuccessComponent } from './Components/oauth-success/oauth-success.component';
 import { OAuthRedirectGuard } from './Guards/o-auth-redirect.guard';
 import { OauthErrorComponent } from './Components/oauth-error/oauth-error.component';
+import { RegisterComponent } from './Components/register/register.component';
 
 const routes: Routes = [
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
   },
   {
     path: 'oauth-success',
     canActivate: [OAuthRedirectGuard],
-    component: OAuthSuccessComponent
+    component: OAuthSuccessComponent,
   },
   {
     path: 'oauth-error',
-    component: OauthErrorComponent
+    component: OauthErrorComponent,
   },
   {
     path: '',
     redirectTo: 'login',
-    pathMatch: 'full'
-  }
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
