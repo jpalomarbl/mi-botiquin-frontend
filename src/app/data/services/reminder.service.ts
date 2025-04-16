7c2fd180e3e656863513d8d3bfe71ced840d67d5
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-16 13:13:49 +0100

    Made reminders Service

--- src/app/data/services/reminder.service.ts
+++ src/app/data/services/reminder.service.ts
@@ -1,11 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
+import { skipWhile, Observable, of } from 'rxjs';
 
 import { environment } from 'src/app/environment/environment';
 import { ReminderDTO } from 'src/app/Models/reminder.dto';
-import { selectUser } from '../selectors/auth.selectors';
+import { selectUser } from 'src/app/auth/selectors/auth.selectors';
+import { AuthStateDTO } from 'src/app/auth/models/auth.dto';
 
 @Injectable({
   providedIn: 'root',
@@ -14,20 +15,16 @@ export class ReminderService {
   private apiUrl = environment.api_url + '/reminder';
   user$ = this.store.select(selectUser);
 
-  constructor(private http: HttpClient, private store: Store) {}
+  constructor(private http: HttpClient, private store: Store<AuthStateDTO>) {}
 
-  getRemindersForToday(): Observable<any> {
-    let userId = 0;
-
-    this.user$.subscribe((user) => {
-      if (user) {
-        userId = user.id;
-      }
-    });
-
-    return this.http.get<ReminderDTO>(`${this.apiUrl}/user/today`, {
+  getRemindersForToday(userId: number): Observable<any> {
+    const response = this.http.get<ReminderDTO>(`${this.apiUrl}/user/today`, {
       withCredentials: true,
-      params: { userId: userId.toString() }
+      params: { userId: userId.toString() },
     });
+
+    // console.log(response)
+
+    return response;
   }
 }
