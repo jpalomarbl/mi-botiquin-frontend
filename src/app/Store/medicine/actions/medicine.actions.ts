b0a6e892791fede85029b3469e0bd488711c4060
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-17 09:13:19 +0100

    Made getRemindersForToday success and error actions

--- src/app/Store/medicine/actions/medicine.actions.ts
+++ src/app/Store/medicine/actions/medicine.actions.ts
@@ -1,5 +1,16 @@
-import { createAction } from '@ngrx/store';
+import { createAction, props } from '@ngrx/store';
+import { ReminderDTO } from 'src/app/Models/reminder.dto';
 
 export const getUserRemindersForToday = createAction(
   '[Medicine] Get User Reminders For Today'
+);
+
+export const getUserRemindersForTodaySuccess = createAction(
+  '[Medicine] Get User Reminders For Today Success',
+  props<{ reminders: ReminderDTO[] }>()
+);
+
+export const getUserRemindersForTodayError = createAction(
+  '[Medicine] Get User Reminders For Today Error',
+  props<{ error: string }>()
 );
