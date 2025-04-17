917e8bf6e7d93e9ce75e05db8da67d8153170365
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-17 11:21:14 +0100

    Added reminders field to medicineStateDTO

--- src/app/Models/medicineState.dto.ts
+++ src/app/Models/medicineState.dto.ts
@@ -1,7 +1,9 @@
 import { MedicineKitDTO } from "./medicineKit.dto";
+import { ReminderDTO } from "./reminder.dto";
 
 export interface MedicineStateDTO {
   medicineKits: MedicineKitDTO[];
+  reminders: ReminderDTO[];
   loading: boolean;
   loaded: boolean;
   error: string | null;
