99db42eaca946bc8fd3e9f276b7cc006c7d420f7
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-17 11:27:39 +0100

    Changed reminder service method name

--- src/app/Store/medicine/services/reminder.service.ts
+++ src/app/Store/medicine/services/reminder.service.ts
@@ -6,7 +6,8 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/app/environment/environment';
 import { ReminderDTO } from 'src/app/Models/reminder.dto';
 import { selectUser } from 'src/app/Store/auth/selectors/auth.selectors';
-import { AuthStateDTO } from '../../../Models/auth.dto';
+import { AuthStateDTO } from 'src/app/Models/authState.dto';
+import { GlobalStateDTO } from 'src/app/Models/globalState.dto';
 
 @Injectable({
   providedIn: 'root',
@@ -15,9 +16,9 @@ export class ReminderService {
   private apiUrl = environment.api_url + '/reminder';
   user$ = this.store.select(selectUser);
 
-  constructor(private http: HttpClient, private store: Store<AuthStateDTO>) {}
+  constructor(private http: HttpClient, private store: Store<GlobalStateDTO>) {}
 
-  getRemindersForToday(userId: number): Observable<any> {
+  getUserRemindersForToday(userId: number): Observable<any> {
     const response = this.http.get<ReminderDTO>(`${this.apiUrl}/user/today`, {
       withCredentials: true,
       params: { userId: userId.toString() },
