c516095a6b1907d991dc1d1b4893dc369281a440
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-17 11:50:28 +0100

    Used new reminder selector and simplified implementation with asyncPipe

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,33 +1,29 @@
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { filter } from 'rxjs';
 
 import { GlobalStateDTO } from 'src/app/Models/globalState.dto';
-import { ReminderDTO } from 'src/app/Models/reminder.dto';
 import { UserDTO } from 'src/app/Models/user.dto';
-import {
-  selectAuthLoaded,
-  selectUser,
-} from 'src/app/Store/auth/selectors/auth.selectors';
+
+import { selectUser } from 'src/app/Store/auth/selectors/auth.selectors';
+import * as medicineSelectors from 'src/app/Store/medicine/selectors/reminder.selectors';
+
 import * as medicineActions from 'src/app/Store/medicine/actions/medicine.actions';
 import { ReminderService } from 'src/app/Store/medicine/services/reminder.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AsyncPipe],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  reminders: ReminderDTO[] = [];
+  todaysReminders$ = this.store.select(medicineSelectors.selectReminders);
   user$ = this.store.select(selectUser);
-  loaded$ = this.store.select(selectAuthLoaded);
-  userId: number = 0;
 
   constructor(
-    private reminderService: ReminderService,
     private store: Store<GlobalStateDTO>
   ) {}
   ngOnInit() {
