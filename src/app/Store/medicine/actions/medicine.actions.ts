8697e0b73312796569af4a3027458adeeed64eaf
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-22 10:12:38 +0100

    Made fetchMedicineKits actions

--- src/app/Store/medicine/actions/medicine.actions.ts
+++ src/app/Store/medicine/actions/medicine.actions.ts
@@ -1,5 +1,6 @@
 import { createAction, props } from '@ngrx/store';
 import { ReminderDTO } from 'src/app/Models/reminder.dto';
+import { MedicineKitDTO } from 'src/app/Models/medicineKit.dto';
 
 export const fetchUserRemindersForToday = createAction(
   '[Medicine] Fetch User Reminders For Today From API',
@@ -13,5 +14,20 @@ export const fetchUserRemindersForTodaySuccess = createAction(
 
 export const fetchUserRemindersForTodayError = createAction(
   '[Medicine] Fetch User Reminders For Today From API Error',
+  props<{ error: string }>()
+);
+
+export const fetchUserMedicineKits = createAction(
+  '[Medicine] Fetch User Medicine Kits From API',
+  props<{ userId: number }>()
+);
+
+export const fetchUserMedicineKitsSuccess = createAction(
+  '[Medicine] Fetch User Medicine Kits From API Success',
+  props<{ medicineKits: MedicineKitDTO[] }>()
+);
+
+export const fetchUserMedicineKitsError = createAction(
+  '[Medicine] Fetch User Medicine Kits From API Error',
   props<{ error: string }>()
 );
