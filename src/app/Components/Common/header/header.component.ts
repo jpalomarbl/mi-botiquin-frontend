9ceb3b9c9ceb540b62adae3a6ee4e6d56ecb1c86
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-22 20:32:56 +0100

    BETA: Finished implementation of header

--- src/app/Components/Common/header/header.component.ts
+++ src/app/Components/Common/header/header.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'custom-header',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule],
+  imports: [MatIconModule, MatButtonModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Input() hasBackButton: boolean = true;
+  @Input() backButtonDirection: string = 'back';
+
+  @Input() title: string = 'Mi Botiquín';
+
+  private buttonDirection: string = '';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    console.log(window.history)
+  }
+
+  backButtonRedirect() {
+    if (this.backButtonDirection === 'back') {
+      history.back();
+    } else {
+      this.router.navigate([this.backButtonDirection]);
+    }
+  }
+}
