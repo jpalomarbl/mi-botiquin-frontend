d6e50a7dd749e73bd682d34e66d73673c9bbc17b
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-24 19:41:14 +0100

    Added header and footer to RemindersList page

--- src/app/Components/Pages/reminders-list/reminders-list.component.ts
+++ src/app/Components/Pages/reminders-list/reminders-list.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 
+import { HeaderComponent } from '../../Common/header/header.component';
+import { FooterComponent } from '../../Common/footer/footer.component';
+
 @Component({
   selector: 'app-reminders-list',
+  standalone: true,
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './reminders-list.component.html',
   styleUrls: ['./reminders-list.component.scss']
 })
