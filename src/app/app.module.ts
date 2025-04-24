c0f20978d592dffd28322deb3ff32bb1ac7d4c8b
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-24 16:37:58 +0100

    Created footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,9 +18,10 @@ import { authReducer } from './Store/auth/reducers/auth.reducer';
 import { MedicineKitEffects } from './Store/medicine/effects/medicineKit.effects';
 import { ReminderEffects } from './Store/medicine/effects/reminder.effects';
 import { medicineReducer } from './Store/medicine/reducer/medicine.reducer';
+import { FooterComponent } from './Components/Common/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, OAuthSuccessComponent, OauthErrorComponent],
+  declarations: [AppComponent, OAuthSuccessComponent, OauthErrorComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
