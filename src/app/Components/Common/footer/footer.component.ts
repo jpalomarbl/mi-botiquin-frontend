7eab325a004ccb7073a01fb4738e550d834b78f5
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-24 16:58:35 +0100

    BETA: Finished footer

--- src/app/Components/Common/footer/footer.component.ts
+++ src/app/Components/Common/footer/footer.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 @Component({
-  selector: 'app-footer',
+  selector: 'custom-footer',
+  standalone: true,
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
