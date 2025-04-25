9dc20f751b2ef7201bae0d7c61551a4326c47f08
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-25 11:58:39 +0100

    FInished date-format pipe

--- src/app/Pipes/date-format.pipe.ts
+++ src/app/Pipes/date-format.pipe.ts
@@ -1,12 +1,36 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'dateFormat'
+  name: 'dateFormat',
 })
 export class DateFormatPipe implements PipeTransform {
+  private days = ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'];
+  private months = [
+    'Ene',
+    'Feb',
+    'Mar',
+    'Abr',
+    'May',
+    'Jun',
+    'Jul',
+    'Ago',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dic',
+  ];
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: Date | string | null | undefined): string {
+    if (!value) return '';
+
+    const date = new Date(value);
+
+    if (isNaN(date.getTime())) return '';
+
+    const dayOfWeek = this.days[date.getDay()];
+    const dayOfMonth = date.getDate();
+    const month = this.months[date.getMonth()];
+
+    return `${dayOfWeek}, ${dayOfMonth} de ${month}`;
   }
-
 }
