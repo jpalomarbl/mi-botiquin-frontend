162d3c60f367078d3b0998eb0585180932608962
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-25 11:56:28 +0100

    Created date-format pipe

--- src/app/Pipes/next-dose.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NextDosePipe } from './next-dose.pipe';
-
-describe('NextDosePipe', () => {
-  it('create an instance', () => {
-    const pipe = new NextDosePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
