f6a031ded57bb2ed654ab3171e6d3b9a9a0e8c6f
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-25 09:57:01 +0100

    Added route params to remindersListComponent

--- src/app/Components/Pages/reminders-list/reminders-list.component.ts
+++ src/app/Components/Pages/reminders-list/reminders-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { filter } from 'rxjs';
 
@@ -18,14 +19,38 @@ import { HeaderComponent } from '../../Common/header/header.component';
 })
 export class RemindersListComponent {
   user$ = this.store.select(selectUser);
+  wentBack = false;
+  wentForward = false;
+  amount = null;
+  day = new Date();
 
-  constructor(private store: Store<GlobalStateDTO>) {}
+  constructor(
+    private store: Store<GlobalStateDTO>,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    if (this.router.url === '/remindersList/forward') this.wentForward = true;
+    else if (this.router.url === '/remindersList/back') this.wentBack = true;
+
+    this.route.queryParams.subscribe(params => {
+      this.amount = params['amount'];
+
+      if (this.amount) {
+        if (this.wentBack) {
+          this.day.setDate(this.day.getDate() - (+this.amount));
+        } else if (this.wentForward) {
+          this.day.setDate(this.day.getDate() + (+this.amount));
+        }
+      }
+    });
+
     this.user$.pipe(filter((user) => user !== null)).subscribe((user) => {
       this.store.dispatch(
         reminderActions.fetchAllUserReminders({
           userId: (user! as UserDTO).id,
+          day: this.day,
         })
       );
     });
