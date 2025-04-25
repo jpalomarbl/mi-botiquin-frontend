ee7ff5318ba2a041c054c87f22089b4f7dc8316c
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-25 20:50:00 +0100

    Made shortenTextPipe

---
+++ src/app/Pipes/shorten-text.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'shortenText'
+})
+export class ShortenTextPipe implements PipeTransform {
+
+  transform(value: string | null | undefined): string {
+    if (!value) return '';
+    else return value.slice(0, 30) + "\u2026";
+  }
+}
