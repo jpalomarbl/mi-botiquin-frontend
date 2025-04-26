990f3aaf9de594fe9c251d26b09d67eb476a368d
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-26 08:22:00 +0100

    Added attribute for relationships in AuthDTO

--- src/app/Models/authState.dto.ts
+++ src/app/Models/authState.dto.ts
@@ -2,6 +2,7 @@ import { UserDTO } from "./user.dto";
 
 export interface AuthStateDTO {
   user: UserDTO | null;
+  relationships: UserDTO[] | null;
   loading: boolean;
   loaded: boolean;
   error: string | null;
