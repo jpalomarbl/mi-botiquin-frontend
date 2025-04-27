fb732d2679af050fe2ba18d7a5489a07c597a53c
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-27 12:31:21 +0100

    Changed shortenText pipe to avoid adding '...' if text is short

--- src/app/Pipes/shorten-text.pipe.ts
+++ src/app/Pipes/shorten-text.pipe.ts
@@ -8,6 +8,7 @@ export class ShortenTextPipe implements PipeTransform {
 
   transform(value: string | null | undefined, characers: number = 30): string {
     if (!value) return '';
+    else if (value.length <= characers) return value;
     else return value.slice(0, characers) + "\u2026";
   }
 }
