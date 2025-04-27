7dc5d5f0bfb78070c32337687156b80cd5f25a59
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-28 00:25:01 +0100

    Added data fetching to medicineKitsListComponent

--- src/app/Components/Pages/medicine-kits-list/medicine-kits-list.component.ts
+++ src/app/Components/Pages/medicine-kits-list/medicine-kits-list.component.ts
@@ -1,15 +1,52 @@
+// Angular
 import { Component } from '@angular/core';
 
+// Rxjs and Redux
+import { Observable } from 'rxjs';
+
+// Store
+import { Store } from '@ngrx/store';
+import { selectUser } from 'src/app/Store/auth/selectors/auth.selectors';
+import * as medicineKitActions from 'src/app/Store/medicine/actions/medicineKits.actions';
+import * as medicineSelectors from 'src/app/Store/medicine/selectors/medicine.selectors';
+
 //Components
-import { HeaderComponent } from '../../Common/header/header.component';
 import { FooterComponent } from '../../Common/footer/footer.component';
+import { HeaderComponent } from '../../Common/header/header.component';
+
+// Data models
+import { GlobalStateDTO } from 'src/app/Models/globalState.dto';
+import { MedicineKitDTO } from 'src/app/Models/medicineKit.dto';
+import { UserDTO } from 'src/app/Models/user.dto';
 
 @Component({
   selector: 'app-medicine-kits-list',
   standalone: true,
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './medicine-kits-list.component.html',
-  styleUrls: ['./medicine-kits-list.component.scss']
+  styleUrls: ['./medicine-kits-list.component.scss'],
 })
 export class MedicineKitsListComponent {
+  user$: Observable<UserDTO | null>;
+  medicineKits$: Observable<MedicineKitDTO[]>;
 
+  constructor(private store: Store<GlobalStateDTO>) {
+    this.user$ = this.store.select(selectUser);
+    this.medicineKits$ = this.store.select(
+      medicineSelectors.selectMedicineKits
+    );
+  }
+
+  ngOnInit(): void {
+    this.user$.subscribe((user: UserDTO | null) => {
+      if (user) {
+        this.store.dispatch(
+          medicineKitActions.fetchUserMedicineKits({
+            userId: user.id,
+            role: user.role,
+          })
+        );
+      }
+    });
+  }
 }
