ac1621009bbd8f07c355a568e3af7b9372fc83cc
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-28 08:27:47 +0100

    Made medicineKitDetailsComponent standalone

--- src/app/Components/Pages/medicine-kit-details/medicine-kit-details.component.ts
+++ src/app/Components/Pages/medicine-kit-details/medicine-kit-details.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 
+import { HeaderComponent } from '../../Common/header/header.component';
+import { FooterComponent } from '../../Common/footer/footer.component';
+
 @Component({
   selector: 'app-medicine-kit-details',
+  standalone: true,
+  imports: [],
   templateUrl: './medicine-kit-details.component.html',
   styleUrls: ['./medicine-kit-details.component.scss']
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,10 +18,9 @@ import { authReducer } from './Store/auth/reducers/auth.reducer';
 import { MedicineKitEffects } from './Store/medicine/effects/medicineKit.effects';
 import { ReminderEffects } from './Store/medicine/effects/reminder.effects';
 import { medicineReducer } from './Store/medicine/reducer/medicine.reducer';
-import { MedicineKitDetailsComponent } from './Components/Pages/medicine-kit-details/medicine-kit-details.component';
 
 @NgModule({
-  declarations: [AppComponent, OAuthSuccessComponent, OauthErrorComponent, MedicineKitDetailsComponent],
+  declarations: [AppComponent, OAuthSuccessComponent, OauthErrorComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
