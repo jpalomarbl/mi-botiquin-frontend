cb494c983962176d0250ffbe0d6b4e4be2510e5a
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-29 22:33:01 +0100

    Made addMedicienComponent standalone

--- src/app/Components/Pages/add-medicine/add-medicine.component.ts
+++ src/app/Components/Pages/add-medicine/add-medicine.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-add-medicine',
+  standalone: true,
+  imports: [],
   templateUrl: './add-medicine.component.html',
   styleUrls: ['./add-medicine.component.scss']
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,10 +18,9 @@ import { authReducer } from './Store/auth/reducers/auth.reducer';
 import { MedicineKitEffects } from './Store/medicine/effects/medicineKit.effects';
 import { ReminderEffects } from './Store/medicine/effects/reminder.effects';
 import { medicineReducer } from './Store/medicine/reducer/medicine.reducer';
-import { AddMedicineComponent } from './Components/Pages/add-medicine/add-medicine.component';
 
 @NgModule({
-  declarations: [AppComponent, OAuthSuccessComponent, OauthErrorComponent, AddMedicineComponent],
+  declarations: [AppComponent, OAuthSuccessComponent, OauthErrorComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
