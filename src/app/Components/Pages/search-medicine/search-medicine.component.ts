2d25eb7369c3661f753707c44105328e15f2a1b3
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-29 20:44:01 +0100

    medicinesSearch are store on an attribute on searchMedicineComponent

--- src/app/Components/Pages/search-medicine/search-medicine.component.ts
+++ src/app/Components/Pages/search-medicine/search-medicine.component.ts
@@ -1,20 +1,36 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+// Rxjs
+import { Observable, take } from 'rxjs';
 
 // Custom modules
 import { FormsModule } from 'src/app/Modules/forms.module';
 
+// Store
+import { Actions, ofType } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
+import * as medicineKitActions from 'src/app/Store/medicine/actions/medicineKits.actions';
+import * as medicineKitSelectors from 'src/app/Store/medicine/selectors/medicine.selectors';
+
 // Components
-import { HeaderComponent } from '../../Common/header/header.component';
 import { FooterComponent } from '../../Common/footer/footer.component';
+import { HeaderComponent } from '../../Common/header/header.component';
+
+// Models
+import { GlobalStateDTO } from 'src/app/Models/globalState.dto';
+import { MedicineDTO } from 'src/app/Models/medicine.dto';
+
+// Angular material
+import { ScrollingModule } from '@angular/cdk/scrolling';
+
 
 @Component({
   selector: 'app-search-medicine',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, FormsModule],
+  imports: [HeaderComponent, FooterComponent, FormsModule, ScrollingModule],
   templateUrl: './search-medicine.component.html',
-  styleUrls: ['./search-medicine.component.scss']
+  styleUrls: ['./search-medicine.component.scss'],
 })
 export class SearchMedicineComponent {
   medicine: FormControl;
@@ -22,13 +38,39 @@ export class SearchMedicineComponent {
 
   medicineName: string;
 
-  constructor() {
+  medicinesSearch: MedicineDTO[];
+  medicinesSearch$: Observable<MedicineDTO[]>;
+
+  constructor(private store: Store<GlobalStateDTO>, private actions$: Actions) {
     this.medicineName = '';
+    this.medicinesSearch = [];
 
     this.medicine = new FormControl(this.medicineName, Validators.required);
 
     this.medicineForm = new FormGroup({
-      medicine: this.medicine
+      medicine: this.medicine,
     });
+
+    this.medicinesSearch$ = this.store.select(
+      medicineKitSelectors.selectMedicinesSearch
+    );
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(
+      medicineKitActions.fetchMedicinesCIMA({ medicineName: 'ibuprofeno' })
+    );
+
+    this.actions$
+      .pipe(
+        ofType(medicineKitActions.fetchMedicinesCIMASuccess),
+        take(1) // Para autodesuscribirse después de ejecutarse una vez
+      )
+      .subscribe(() => {
+        // Código a ejecutar después de eliminar
+        this.medicinesSearch$.subscribe((medicines: MedicineDTO[]) => {
+          this.medicinesSearch = medicines;
+        });
+      });
   }
 }
