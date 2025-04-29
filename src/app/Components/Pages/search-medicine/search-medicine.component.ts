7369cd626bc6b88f19058baefdee5d23f23fdcbc
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-29 19:38:43 +0100

    Finished basic interface of searchMedicineComponent

--- src/app/Components/Pages/search-medicine/search-medicine.component.ts
+++ src/app/Components/Pages/search-medicine/search-medicine.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
+
+// Custom modules
+import { FormsModule } from 'src/app/Modules/forms.module';
 
 // Components
 import { HeaderComponent } from '../../Common/header/header.component';
@@ -7,10 +12,23 @@ import { FooterComponent } from '../../Common/footer/footer.component';
 @Component({
   selector: 'app-search-medicine',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, FormsModule],
   templateUrl: './search-medicine.component.html',
   styleUrls: ['./search-medicine.component.scss']
 })
 export class SearchMedicineComponent {
+  medicine: FormControl;
+  medicineForm: FormGroup;
 
+  medicineName: string;
+
+  constructor() {
+    this.medicineName = '';
+
+    this.medicine = new FormControl(this.medicineName, Validators.required);
+
+    this.medicineForm = new FormGroup({
+      medicine: this.medicine
+    });
+  }
 }
