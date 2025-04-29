7a85ad204435287b2849817e6134c33722f9f56c
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-29 22:17:18 +0100

    Generated addMedicineComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,9 +18,10 @@ import { authReducer } from './Store/auth/reducers/auth.reducer';
 import { MedicineKitEffects } from './Store/medicine/effects/medicineKit.effects';
 import { ReminderEffects } from './Store/medicine/effects/reminder.effects';
 import { medicineReducer } from './Store/medicine/reducer/medicine.reducer';
+import { AddMedicineComponent } from './Components/Pages/add-medicine/add-medicine.component';
 
 @NgModule({
-  declarations: [AppComponent, OAuthSuccessComponent, OauthErrorComponent],
+  declarations: [AppComponent, OAuthSuccessComponent, OauthErrorComponent, AddMedicineComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
