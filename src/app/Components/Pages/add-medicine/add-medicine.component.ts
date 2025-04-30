d76a33e54f6a9e7b69fd43284c918df209bb8a8f
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-04-30 18:36:27 +0100

    Added validations to forms on addMedicineComponent

--- src/app/Components/Pages/add-medicine/add-medicine.component.ts
+++ src/app/Components/Pages/add-medicine/add-medicine.component.ts
@@ -1,6 +1,6 @@
 // Angular
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 
 // Angular material
@@ -15,6 +15,7 @@ import { FooterComponent } from '../../Common/footer/footer.component';
 import { HeaderComponent } from '../../Common/header/header.component';
 
 // Store
+import { Store } from '@ngrx/store';
 import { MedicineKitService } from 'src/app/Services/medicineKit.service';
 
 // Models
@@ -83,7 +84,8 @@ export class AddMedicineComponent {
 
   constructor(
     private route: ActivatedRoute,
-    private medicineKitService: MedicineKitService
+    private medicineKitService: MedicineKitService,
+    private store: Store
   ) {
     this.medicine = JSON.parse(
       decodeURIComponent(this.route.snapshot.params['medicine'])
@@ -106,17 +108,18 @@ export class AddMedicineComponent {
 
     this.addReminder = false;
     this.medicineUnitsArray = [];
-    this.expirationDate = new FormControl(this.medicine.expirationDate);
-    this.amountMedicine = new FormControl(this.medicine.amount);
-    this.unitMedicine = new FormControl(this.medicine.unit);
 
-    this.amountReminder = new FormControl(this.reminder.amount);
-    this.frequency = new FormControl(this.reminder.frequency);
-    this.frequencyUnit = new FormControl(this.reminder.frequencyUnit);
-    this.startDate = new FormControl(this.startDateData);
-    this.startTime = new FormControl(this.startTimeData);
-    this.finishDate = new FormControl(this.finishDateData);
-    this.finishTime = new FormControl(this.finishTimeData);
+    this.expirationDate = new FormControl(this.medicine.expirationDate, Validators.required);
+    this.amountMedicine = new FormControl(this.medicine.amount, [Validators.required, Validators.min(0)]);
+    this.unitMedicine = new FormControl(this.medicine.unit, Validators.required);
+
+    this.amountReminder = new FormControl(this.reminder.amount, [Validators.required, Validators.min(1)]);
+    this.frequency = new FormControl(this.reminder.frequency, [Validators.required, Validators.min(1)]);
+    this.frequencyUnit = new FormControl(this.reminder.frequencyUnit, Validators.required);
+    this.startDate = new FormControl(this.startDateData, Validators.required);
+    this.startTime = new FormControl(this.startTimeData, Validators.required);
+    this.finishDate = new FormControl(this.finishDateData || null, { nonNullable: true });
+    this.finishTime = new FormControl(this.finishTimeData|| null, { nonNullable: true });
 
     this.medicineForm = new FormGroup({
       expirationDate: this.expirationDate,
@@ -141,7 +144,56 @@ export class AddMedicineComponent {
         this.medicineUnitsArray.push(Object.entries(item[1])[0]);
       });
     });
+
+    const medicine: MedicineDTO = {
+      name: 'medicinetest3',
+      unit: 'medicinetest3',
+      amount: 1,
+      dose: 1,
+      expirationDate: new Date(),
+      nregistro: 1,
+    };
+
+    const medicineKitId = 2;
+
+    const reminder = {
+      frequency: 1,
+      frequencyUnit: 'horas',
+      start: new Date(),
+      finish: new Date(),
+      amount: 1,
+      medicineUnit: 'medicinetest3',
+    };
+
+    // this.store.dispatch(medicineKitActions.addMedicine({ medicine: medicine, reminder: reminder, medicineKitId: 2}));
+
+    // this.reminderService.addReminder(this.reminder, 30).subscribe((response) => {
+    //   console.log(response)
+    // })
   }
 
-  submitForms(): void {}
+  submitForms(): void {
+    console.log('expirationDate:', this.expirationDate.valid);
+    console.log('amountMedicine:', this.amountMedicine.valid);
+    console.log('unitMedicine:', this.unitMedicine.valid);
+
+    // const start = this.startDate.value;
+    // start.setHours(+this.startTime.value.slice(0, 2));
+    // start.setMinutes(+this.startTime.value.slice(3, 5))
+
+    // const finish = this.finishDate.value;
+    // finish.setHours(+this.finishTime.value.slice(0, 2));
+    // finish.setMinutes(+this.finishTime.value.slice(3, 5))
+
+    console.log('amountReminder:', this.amountReminder.valid);
+    console.log('frequency:', this.frequency.valid);
+    console.log('frequencyUnit:', this.frequencyUnit.valid);
+    console.log('start:', this.startDate.valid);
+    console.log('startTime:', this.startTime.valid);
+    console.log('finish:', this.finishDate.valid);
+    console.log('finishTime:', this.finishTime.errors);
+    console.log('finishTime:', this.finishTime.errors);
+    console.log("MEDICINE FORM", this.medicineForm.valid)
+    console.log("REMINDER FORM", this.reminderForm.valid)
+  }
 }
