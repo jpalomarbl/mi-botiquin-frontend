ab7a3c4d6e02c6edd6423dbd732318af9e736995
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-05-06 13:47:15 +0100

    Generated error dialog component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,9 +18,10 @@ import { authReducer } from './Store/auth/reducers/auth.reducer';
 import { MedicineKitEffects } from './Store/medicine/effects/medicineKit.effects';
 import { ReminderEffects } from './Store/medicine/effects/reminder.effects';
 import { medicineReducer } from './Store/medicine/reducer/medicine.reducer';
+import { ErrorDialogComponent } from './Components/Common/error-dialog/error-dialog.component';
 
 @NgModule({
-  declarations: [AppComponent, OAuthSuccessComponent, OauthErrorComponent],
+  declarations: [AppComponent, OAuthSuccessComponent, OauthErrorComponent, ErrorDialogComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
