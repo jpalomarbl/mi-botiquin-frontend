c4c2f2a30b0102b261c59ffc78748cd391094c08
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-05-06 18:56:30 +0100

    Made errorService

---
+++ src/app/Services/error.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from '../Components/Common/error-dialog/error-dialog.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ErrorService {
+
+  constructor(public errorDialog: MatDialog) { }
+
+  openErrorDialog(errorMsg: string): void {
+      this.errorDialog.open(ErrorDialogComponent, {
+        data: {
+          errorMsg: errorMsg,
+        },
+      });
+    }
+}
