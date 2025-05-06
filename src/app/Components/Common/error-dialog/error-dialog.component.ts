e06b69a126e732738f1e690caddddb08f3f01b07
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-05-06 14:10:03 +0100

    Made basic structure of errorDialogComponent

--- src/app/Components/Common/error-dialog/error-dialog.component.ts
+++ src/app/Components/Common/error-dialog/error-dialog.component.ts
@@ -1,16 +1,17 @@
 // Angular
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 
 // Angular Material
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-error-dialog',
   standalone: true,
-  imports: [MatDialogModule],
+  imports: [],
   templateUrl: './error-dialog.component.html',
   styleUrls: ['./error-dialog.component.scss']
 })
 export class ErrorDialogComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public errorMsg: string) {}
 
 }
