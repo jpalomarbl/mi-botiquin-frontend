654ca609caf6308029297b530e5862581628dab5
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-05-12 13:43:18 +0100

    Allowed userConfig to be a route on footerComponent

--- src/app/Components/Common/footer/footer.component.ts
+++ src/app/Components/Common/footer/footer.component.ts
@@ -1,24 +1,27 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'custom-footer',
   standalone: true,
   imports: [MatIconModule, MatButtonModule],
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
   constructor(private router: Router) {}
 
   navigate(route: string): void {
-    if (route === '/' || route === 'remindersList' || route === 'medicineKitsList') {
+    if (
+      route === '/' ||
+      route === 'remindersList' ||
+      route === 'medicineKitsList' ||
+      route === 'userConfig'
+    ) {
       this.router.navigate([route]);
     }
-
-
   }
 }
