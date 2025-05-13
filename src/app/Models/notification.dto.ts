513d421d5f3eda491e103018154c93301271e4e3
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-05-13 12:50:52 +0100

    Renamed new NotificationDTO

--- src/app/Models/notification.dto.ts
+++ src/app/Models/notification.dto.ts
@@ -1,12 +1,5 @@
-export interface NotificationDTO {
-  type: string;
-  medicineId: number;
-  medicineName: string;
-  medicineKitId: number;
-  medicineKitName: string;
-}
 
-export interface Notification1DTO {
+export interface NotificationDTO {
   // If type is 'expired', id1: medicineId, id2: medicineKitId
   // If type is 'relationship request', id1: requesterId, id2: receiverId
   id1: number;
@@ -14,12 +7,12 @@ export interface Notification1DTO {
   type: string;
 }
 
-export interface expirationNotificationDTO extends Notification1DTO {
+export interface expirationNotificationDTO extends NotificationDTO {
   medicineName: string;
   medicineKitName: string;
 }
 
-export interface relationshipRequestNotificationDTO extends Notification1DTO {
+export interface relationshipRequestNotificationDTO extends NotificationDTO {
   requesterFirstName: string;
   requesterLastName: string;
   requesterRole: string;
