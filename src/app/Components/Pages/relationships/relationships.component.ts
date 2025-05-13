ad0ec2ac08169eb7fd172619bdad7dd69db03df6
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-05-14 00:07:49 +0100

    Finished relationshipsComponent view

--- src/app/Components/Pages/relationships/relationships.component.ts
+++ src/app/Components/Pages/relationships/relationships.component.ts
@@ -6,30 +6,70 @@ import { Observable } from 'rxjs';
 
 // Store
 import { Store } from '@ngrx/store';
-import {
-  selectUser,
-  selectUserRelationships,
-} from 'src/app/Store/auth/selectors/auth.selectors';
 import * as userRelationshipsActions from 'src/app/Store/auth/actions/userRelationships.actions';
+import * as authSelectors from 'src/app/Store/auth/selectors/auth.selectors';
+
+// Components
+import { FooterComponent } from '../../Common/footer/footer.component';
+import { HeaderComponent } from '../../Common/header/header.component';
+
+// Custom modules
+import { FormControl, FormGroup } from '@angular/forms';
+import { AngularMaterialModule } from 'src/app/Modules/angular-material.module';
+import { FormsModule } from 'src/app/Modules/forms.module';
 
 // Models
 import { GlobalStateDTO } from 'src/app/Models/globalState.dto';
 import { UserDTO } from 'src/app/Models/user.dto';
+
 @Component({
   selector: 'app-relationships',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    FormsModule,
+    AngularMaterialModule,
+  ],
   templateUrl: './relationships.component.html',
   styleUrls: ['./relationships.component.scss'],
 })
 export class RelationshipsComponent {
   userRelationships$: Observable<UserDTO[] | null>;
   user$: Observable<UserDTO | null>;
+  userSearchResults$: Observable<UserDTO[] | null>;
+  loading$: Observable<boolean | null>;
 
   userRole: string;
+  userSearchResults: UserDTO[] | null;
+  userRelationships: UserDTO[] | null;
+
+  patientSearchString: string;
+
+  patientSearch: FormControl;
+  searchForm: FormGroup;
 
   constructor(private store: Store<GlobalStateDTO>) {
+    this.userRelationships$ = this.store.select(
+      authSelectors.selectUserRelationships
+    );
+    this.userSearchResults$ = this.store.select(
+      authSelectors.selectUserSearchResults
+    );
+    this.user$ = this.store.select(authSelectors.selectUser);
+
+    this.loading$ = this.store.select(authSelectors.selectAuthLoading);
+
+    this.userRelationships = [];
+    this.userSearchResults = [];
+
+    this.patientSearchString = '';
     this.userRole = '';
-    this.userRelationships$ = this.store.select(selectUserRelationships);
-    this.user$ = this.store.select(selectUser);
+
+    this.patientSearch = new FormControl(this.patientSearchString);
+    this.searchForm = new FormGroup({
+      patientSearch: this.patientSearch,
+    });
   }
 
   ngOnInit(): void {
@@ -58,7 +98,31 @@ export class RelationshipsComponent {
             })
           );
         }
+
+        this.userRelationships$.subscribe((relationships) => {
+          console.log('User relationships:', relationships);
+          this.userRelationships = relationships;
+        });
+
+        // this.store.dispatch(notificationActions.sendRelationshipRequest({
+        //   requesterId: user.id,
+        //   receiverId: 8
+        // }));
       }
+    });
+  }
+
+  searchUsers(): void {
+    this.store.dispatch(
+      userRelationshipsActions.searchUsers({
+        searchTerm: this.patientSearch.value,
+      })
+    );
+
+    this.userSearchResults$.subscribe((results) => {
+      console.log('Search results:', results);
+
+      this.userSearchResults = results;
     });
   }
 }
