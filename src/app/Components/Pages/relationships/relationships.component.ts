b56017006a902bc8551ab5ca0be37cbadad9c9da
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-05-13 07:51:39 +0100

    Generated relationshipsComponent

---
+++ src/app/Components/Pages/relationships/relationships.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-relationships',
+  templateUrl: './relationships.component.html',
+  styleUrls: ['./relationships.component.scss']
+})
+export class RelationshipsComponent {
+
+}
