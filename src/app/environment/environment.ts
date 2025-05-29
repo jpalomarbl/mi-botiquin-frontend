0a41ec9f80372bfba77f56f1959d9145f820c1aa
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-05-29 12:15:03 +0100

    Env

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,9 +1,9 @@
-export const environment = {
-  api_url: 'http://localhost:3000/api',
-  websocket_url: 'ws://localhost:8080'
-}
-
 // export const environment = {
-//   api_url: 'https://mi-botiquin-api.onrender.com/api',
-//   websocket_url: 'ws://mi-botiquin-api.onrender.com:8080'
+//   api_url: 'http://localhost:3000/api',
+//   websocket_url: 'ws://localhost:8080'
 // }
+
+export const environment = {
+  api_url: 'https://mi-botiquin-api.onrender.com/api',
+  websocket_url: 'ws://mi-botiquin-api.onrender.com:8080'
+}
