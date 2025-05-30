2623dd317768b5d5e641953a3f4da7b352055374
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2025-05-30 19:36:30 +0100

    Put console.log on oauthredirectGuard

--- src/app/Guards/o-auth-redirect.guard.ts
+++ src/app/Guards/o-auth-redirect.guard.ts
@@ -30,6 +30,8 @@ export class OAuthRedirectGuard implements CanActivate {
     | UrlTree {
     const id = new URLSearchParams(window.location.search).get('id');
 
+    console.log('id', id);
+
     if (id) {
       this.store.dispatch(loginOAuthGetUserInfo({ id: +id }));
     } else {
